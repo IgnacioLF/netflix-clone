b6cfaa99d848708e4dc066a1126567b73019f0de
IgnacioLF <ignaciolopezfeliu@gmail.com>
2023-04-08 11:18:04 +0200

    -added github and google auth

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -4,6 +4,9 @@ import { useCallback, useState } from "react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/router";
 
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+
 const Auth = () => {
   const router = useRouter();
   const [email, setEmail] = useState("");
@@ -87,6 +90,20 @@ const Auth = () => {
             >
               {variant === "login" ? "Login" : "Sign up"}
             </button>
+            <div
+              className="flex flex-row items-center gap-4 mt-8 justify-center"
+              onClick={() => signIn("google", { callbackUrl: "/" })}
+            >
+              <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                <FcGoogle size={30} />
+              </div>
+              <div
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+                onClick={() => signIn("github", { callbackUrl: "/" })}
+              >
+                <FaGithub size={30} />
+              </div>
+            </div>
             <p className="text-neutral-500 mt-12">
               {variant === "login"
                 ? "First time using Netflix?"
