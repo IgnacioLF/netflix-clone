e8306720b69de36645433d9dde7a9f923b02d590
IgnacioLF <ignaciolopezfeliu@gmail.com>
2023-04-09 11:26:55 +0200

    -added profile page and routes protection

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,34 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
 
 export default function Home() {
+  const { data: user } = useCurrentUser();
+
   return (
     <>
       <h1>Netflix clone</h1>
+      <p className="text-white">Logged as : {user?.email}</p>
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
+        Log out
+      </button>
     </>
-  )
+  );
 }
